72b7d474b546cbcc10c7b353132f946abe3829e4
Adam Struthers <support@gamefeed.ca>
2013-11-13 21:24:34 -0500

    Creation of Stacks.java

---
+++ Stack.java
@@ -0,0 +1,42 @@
+import java.util.EmptyStackException;
+import java.util.Vector;
+
+public class Stack < S > extends Vector < S >{
+
+	public synchronized S peek(){
+		int length = size();
+		
+		if(length == 0){
+			throw new EmptyStackException();
+		}
+		return elementAt(len - 1);		
+	}
+			
+	public S pop(){
+		S obj;
+		int length = size();
+		obj = peek();
+		removeElementAt(len - 1);
+		
+		return obj;
+	}
+			
+	public S push(S num){
+		
+		addElement(num);
+		
+		return num;
+	}
+			
+	public boolean isEmpty(){	
+		if(size() == 0){
+			return true;
+		}
+		return false;
+	}
+				
+	public boolean makeEmpty(){
+		return size() == 0;
+	}
+		
+}
